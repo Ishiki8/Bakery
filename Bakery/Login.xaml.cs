1d2b99086a392b4f8d8a1697126d05d280b8a7cf
Dmitriy Spitsyn <yumishiki8@gmail.com>
2023-05-31 00:33:27 +0500

    Add project files.

---
+++ Bakery/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bakery
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public MainWindow mainWindow;
+        public Login(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Password.Password == "12325805")
+            {
+                mainWindow.OpenPage(MainWindow.pages.Menu);
+            }
+            
+        }
+    }
+}
---
+++ Bakery/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bakery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPage(pages.Login);
+        }
+
+        public enum pages
+        {
+            Login,
+            Menu
+        }
+
+        public void OpenPage(pages pages)
+        {
+            if (pages == pages.Login)
+            {
+                MainFrame.Navigate(new Login(this));
+            }
+            else if (pages == pages.Menu)
+            {
+                MainFrame.Navigate(new Menu(this));
+            }  
+        }
+    }
+}
